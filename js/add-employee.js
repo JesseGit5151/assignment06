2cd5c6cebf3ec4f6d8e7218e64090e3f60edc62f
JesseGit5151 <jrodriguez5151yahoo.com>
2021-03-18 14:28:44 -0700

    finished

--- js/add-employee.js
+++ js/add-employee.js
@@ -1,14 +1,44 @@
 // HANDLE THE LOAD EVENT OF THE WINDOW
+window.addEventListener('load', () => {
+    const $ = (id) => {
+        return document.getElementById(id)
+    };
+    let width = 800
+    let height = 700
+    let cancelBtn = $('cancel')
+    let form = $('empForm')
+    
+    window.resizeTo(width,height)
+    window.moveTo(((window.screen.width - width) / 2), ((window.screen.height - height) / 2))
 
-// SET WIDTH AND HEIGHT VARIABLES TO 800 x 700
+    cancelBtn.addEventListener('click', () => {
+        window.close()
+    });
+    form.addEventListener('submit', (e) => {
+        e.preventDefault()
+        let empID = $('id')
+        let fullName = $('name')
+        let ext = $('extension')
+        let email = $('email')
+        let department = $('department')
+        let parentOutput = window.opener.document.getElementById('loginDetails')
+        parentOutput.innerHTML = `ID: ${empID.value}<br>
+                                  Name: ${fullName.value}<br>
+                                  Extension: ${ext.value}<br>
+                                  Email: ${email.value}<br>
+                                  Department: ${department.value}`;
+        window.close()
+    });
+});
+// SET WIDTH AND HEIGHT VARIABLES TO 800 x 700-
 
-// RESIZE THE POPUP TO THE SET WIDTH AND HEIGHT
+// RESIZE THE POPUP TO THE SET WIDTH AND HEIGHT-
 
-// MOVE THE POPUP TO THE MIDDLE OF THE SCREEN
+// MOVE THE POPUP TO THE MIDDLE OF THE SCREEN-
 
-// CREATE A HELPER FUNCTION TO RETRIEVE THE HTML ELEMENTS FROM THE DOM
+// CREATE A HELPER FUNCTION TO RETRIEVE THE HTML ELEMENTS FROM THE DOM-
 
-// HANDLE THE CANCEL BUTTON. WHEN THE USER CLICKS THIS BUTTON, CLOSE THE WINDOW
+// HANDLE THE CANCEL BUTTON. WHEN THE USER CLICKS THIS BUTTON, CLOSE THE WINDOW-
 
 // HANDLE THE SUBMISSION OF THE FORM AND THEN IMMEDIATELY PREVENT THE SUBMISSION
 
--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 // HANDLE THE LOAD EVENT OF THE WINDOW
-
+window.addEventListener('load', () => {
+    let addBtn = document.getElementById('btnAddEmployee')
+    addBtn.addEventListener('click', (e) => {
+        window.open('add-employee.html', 'width=300, height=300', 'resizable=true')
+    });
+});
 // HANDLE THE CLICK EVENT FOR THE BTNADDEMPLOYEE BUTTON
 
 // OPEN THE ADD-EMPLOYEE.HTML PAGE WITHIN A POPUP
